e44ef27a584bd360e61cb39bda9c8690c7f2d68a
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-07 23:55:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let colors = document.querySelector("#colorSelect");
+
+document.querySelector('input[type="button"]')
+	.addEventListener('click',()=>{
+		colors.remove(colors.selectedIndex)
+	});
+					  
+
